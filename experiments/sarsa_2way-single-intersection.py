16921ae579d05d94185f307ac119308f1a40124e
Lucas Alegre <lucasnale@gmail.com>
2020-07-24 14:33:12 -0300

    Removed elapsed time from observation

--- experiments/sarsa_2way-single-intersection.py
+++ experiments/sarsa_2way-single-intersection.py
@@ -21,10 +21,9 @@ if __name__ == '__main__':
     prs = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                   description="""SarsaLambda Single-Intersection""")
     prs.add_argument("-route", dest="route", type=str, default='nets/2way-single-intersection/single-intersection-gen.rou.xml', help="Route definition xml file.\n")
-    prs.add_argument("-a", dest="alpha", type=float, default=0.001, required=False, help="Alpha learning rate.\n")
-    prs.add_argument("-g", dest="gamma", type=float, default=0.95, required=False, help="Gamma discount rate.\n")
+    prs.add_argument("-a", dest="alpha", type=float, default=0.0001, required=False, help="Alpha learning rate.\n")
+    prs.add_argument("-g", dest="gamma", type=float, default=0.99, required=False, help="Gamma discount rate.\n")
     prs.add_argument("-e", dest="epsilon", type=float, default=0.05, required=False, help="Epsilon.\n")
-    prs.add_argument("-lamb", dest="lamb", type=float, default=0.9, required=False, help="Lambda.\n")
     prs.add_argument("-mingreen", dest="min_green", type=int, default=5, required=False, help="Minimum green time.\n")
     prs.add_argument("-maxgreen", dest="max_green", type=int, default=50, required=False, help="Maximum green time.\n")
     prs.add_argument("-gui", action="store_true", default=False, help="Run with visualization on SUMO.\n")
@@ -33,7 +32,7 @@ if __name__ == '__main__':
     prs.add_argument("-runs", dest="runs", type=int, default=1, help="Number of runs.\n")
     args = prs.parse_args()
     experiment_time = str(datetime.now()).split('.')[0]
-    out_csv = 'outputs/2way-single-intersection/sarsa_lambda{}'.format(args.lamb)
+    out_csv = 'outputs/2way-single-intersection/sarsa_lambda'
 
     write_route_file('nets/2way-single-intersection/single-intersection-gen.rou.xml', 400000, 100000)
 
@@ -58,11 +57,10 @@ if __name__ == '__main__':
                             traci.trafficlight.Phase(2, "rrrrryrrrrry")
                             ])
 
-    agent = TrueOnlineSarsaLambda(env.observation_space, env.action_space, alpha=args.alpha, gamma=args.gamma, epsilon=args.epsilon, lamb=args.lamb)
     for run in range(1, args.runs+1):
         obs = env.reset()
-        agent.reset()
-
+        agent = TrueOnlineSarsaLambda(env.observation_space, env.action_space, alpha=args.alpha, gamma=args.gamma, epsilon=args.epsilon, fourier_order=7)
+        
         done = False
         if args.fixed:
             while not done:
@@ -73,12 +71,11 @@ if __name__ == '__main__':
 
                 next_obs, r, done, _ = env.step(action=action)
 
-                agent.learn(state=obs, action=action, reward=r, next_state=next_obs)
+                agent.learn(state=obs, action=action, reward=r, next_state=next_obs, done=done)
 
                 obs = next_obs
 
         env.save_csv(out_csv, run)
-        env.close()
 
 
 
